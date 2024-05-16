eafc6db9005a2ada0e947f29a75ea42fea885af0
EfJot314 <62033206+EfJot314@users.noreply.github.com>
2024-05-16 19:59:12 +0200

    Configure project to cmake

---
+++ CMakeFiles/Asteroids.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/filipjedrzejewski/cpp/Asteroids/src/Game.cpp" "CMakeFiles/Asteroids.dir/src/Game.cpp.o" "gcc" "CMakeFiles/Asteroids.dir/src/Game.cpp.o.d"
+  "/home/filipjedrzejewski/cpp/Asteroids/src/GameEngine.cpp" "CMakeFiles/Asteroids.dir/src/GameEngine.cpp.o" "gcc" "CMakeFiles/Asteroids.dir/src/GameEngine.cpp.o.d"
+  "/home/filipjedrzejewski/cpp/Asteroids/src/Player.cpp" "CMakeFiles/Asteroids.dir/src/Player.cpp.o" "gcc" "CMakeFiles/Asteroids.dir/src/Player.cpp.o.d"
+  "/home/filipjedrzejewski/cpp/Asteroids/src/main.cpp" "CMakeFiles/Asteroids.dir/src/main.cpp.o" "gcc" "CMakeFiles/Asteroids.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/Asteroids.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "Asteroids"
+  "Asteroids.pdb"
+  "CMakeFiles/Asteroids.dir/src/Game.cpp.o"
+  "CMakeFiles/Asteroids.dir/src/Game.cpp.o.d"
+  "CMakeFiles/Asteroids.dir/src/GameEngine.cpp.o"
+  "CMakeFiles/Asteroids.dir/src/GameEngine.cpp.o.d"
+  "CMakeFiles/Asteroids.dir/src/Player.cpp.o"
+  "CMakeFiles/Asteroids.dir/src/Player.cpp.o.d"
+  "CMakeFiles/Asteroids.dir/src/main.cpp.o"
+  "CMakeFiles/Asteroids.dir/src/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Asteroids.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
