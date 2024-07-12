08f98e93fddd8255434205c6a445bc57768714a1
EfJot314 <62033206+EfJot314@users.noreply.github.com>
2024-07-12 16:09:42 +0200

    Project structure refactoring

---
+++ src/visual/Heart.cpp
@@ -0,0 +1,32 @@
+#include "interfaces.h"
+
+Heart::Heart(){}
+
+Heart::Heart(sf::RenderWindow* window, sf::Color color, int x, int y){
+    this->window = window;
+    this->x = x;
+    this->y = y;
+    this->color = color;
+
+    shapeFormation();
+
+    this->shape.setPosition(x, y);
+}
+
+Heart::~Heart(){}
+
+void Heart::shapeFormation(){
+    float halfSize = heartSize / 2;
+
+    shape.setPointCount(4);
+    shape.setPoint(0, sf::Vector2f(halfSize, -halfSize));
+    shape.setPoint(1, sf::Vector2f(halfSize, halfSize));
+    shape.setPoint(2, sf::Vector2f(-halfSize, halfSize));
+    shape.setPoint(3, sf::Vector2f(-halfSize, -halfSize));
+    shape.setFillColor(this->color);
+
+}
+
+void Heart::makeEmpty(){
+    //TODO
+}
---
+++ src/visual/Hearts.cpp
@@ -0,0 +1,44 @@
+#include "interfaces.h"
+
+Hearts::Hearts(){}
+
+Hearts::Hearts(sf::RenderWindow* window, sf::Color color, int n){
+    this->n = n;
+    this->window = window;
+    this->color = color;
+
+    this->x = this->window->getSize().x / 2;
+    this->y = heartsMargin + heartSize/2;
+
+    createHearts();
+}
+
+Hearts::Hearts(sf::RenderWindow* window, sf::Color color, int n, int x, int y){
+    this->n = n;
+    this->window = window;
+    this->color = color;
+
+    this->x = x;
+    this->y = y;
+
+    createHearts();
+}
+
+Hearts::~Hearts(){}
+
+void Hearts::createHearts(){
+    float width = this->n * heartSize + (this->n - 1) * heartsMargin;
+    float dx = -width / 2;
+
+    for(int i=0;i<this->n;i++){
+        int hx = this->x + dx + i * (heartSize + heartsMargin) + heartSize / 2;
+        int hy = this->y;
+        this->hearts.push_back(Heart(this->window, this->color, hx, hy));
+    }
+}
+
+void Hearts::draw(){
+    for(int i=0;i<this->hearts.size();i++){
+        this->hearts[i].draw();
+    }
+}
